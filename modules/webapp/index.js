4a8c0ff284e9ea1ae33af1f33339a2d2f6866e8a
charliebr0wn <cdelaorden@gmail.com>
2016-01-05 18:00:36 +0100

    Fix CORS headers

--- modules/webapp/index.js
+++ modules/webapp/index.js
@@ -16,7 +16,7 @@ function WebAppFactory(config){
   //Enable cross
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
   app.use((err,req,res,next) => {
